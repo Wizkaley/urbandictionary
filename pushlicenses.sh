bb764eb1e99485be33a7cba744aa089e9a777184
Eshan Kaley <kaleyeshan@gmail.com>
2021-05-18 09:49:03 +0530

    Update pushlicenses.sh

--- pushlicenses.sh
+++ pushlicenses.sh
@@ -5,9 +5,9 @@
 
 # variables
 # ex. "https://{YOUR_HOST_HERE}"
-host=""$CUSTOM_HOST"" 
+export host=""$CUSTOM_HOST"" 
 # ex. "YOUR_TOKEN_HERE"
-token=""$CUSTOM_TOKEN""
+export token=""$CUSTOM_TOKEN""
 
 # We somehow need to read the list of allowed/permitted and denied/prohibited licenses in these variables
 # either using a file or fetch from an api.
