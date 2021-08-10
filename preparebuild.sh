e0ad87d00b3036c2f229142d7bb9b78dc94a3827
Eshan Kaley <kaleyeshan@gmail.com>
2021-08-10 16:41:44 +0530

    Update preparebuild.sh

--- preparebuild.sh
+++ preparebuild.sh
@@ -20,9 +20,9 @@ echo repourl :: $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
     if [[ $event == "push" ]];then
         pull_request_number="NIL"
         # go run $GOPATH/src/github.kyndryl.net/MCMP-DevOps-Intelligence/dash_deploy/byobscript/postBuildsToDI.go ${hosts[i]} ${tokens[i]} $serviceName $BUILD_RUNID $providerHref $status $duration $builtat $TRAVIS_BRANCH $GITHUB_EVENT_NAME $pull_request_number $TRAVIS_COMMIT "$BUILD_ENGINE" $GITHUB_SERVER_URL $repourl
-        ./buildscript/postBuildsToDI $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $pull_request_number $13 "$14" $15 $16
+        ./buildscript/postBuildsToDI $2 $3 $SERVICE_NAME $BUILD_RUN_ID "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" $4 "$(($ELAPSED*1000000000))" "$(date --utc +%FT%T.%3NZ)" $BUILD_BRANCH $GITHUB_EVENT_NAME $pull_request_number $BUILD_COMMIT "$BUILD_ENGINE" $GITHUB_SERVER_URL "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY"
     elif [[ $event == "pull_request" ]];then
         # go run $GOPATH/src/github.kyndryl.net/MCMP-DevOps-Intelligence/dash_deploy/byobscript/postBuildsToDI.go ${hosts[i]} ${tokens[i]} $serviceName $BUILD_RUNID $providerHref $status $duration $builtat $TRAVIS_PULL_REQUEST_BRANCH $GITHUB_EVENT_NAME $TRAVIS_PULL_REQUEST $TRAVIS_COMMIT "$BUILD_ENGINE" $GITHUB_SERVER_URL $repourl
-        ./buildscript/postBuildsToDI $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 "$14" $15 $16
+        ./buildscript/postBuildsToDI $2 $3 $SERVICE_NAME $BUILD_RUN_ID "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" $4 "$(($ELAPSED*1000000000))" "$(date --utc +%FT%T.%3NZ)" $BUILD_BRANCH $GITHUB_EVENT_NAME $PULL_REQUEST_NUMBER $BUILD_COMMIT "$BUILD_ENGINE" $GITHUB_SERVER_URL "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY"
     fi
 # done
