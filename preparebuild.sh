4281c9f27ed6a1bc22ff75bb4875cc0e7ce40453
Eshan Kaley <kaleyeshan@gmail.com>
2021-08-10 16:24:21 +0530

    Create preparebuild.sh

--- preparebuild.sh
+++ preparebuild.sh
@@ -3,7 +3,12 @@
 
 event=$1
 echo Event :: $GITHUB_EVENT_NAME
-echo Repo Name :: ${{ github.event.repositroy.name }}
+echo service name :: $SERVICE_NAME
+echo built at :: $(date --utc +%FT%T.%3NZ)
+echo providerhref :: $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID
+echo duration :: $(($ELAPSED*1000000000))
+echo builtat $(date --utc +%FT%T.%3NZ)
+echo repourl :: $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
 # echo $DEV_SECOPS_HOST_TOKEN >> byob_secrets.json
 # hosts=($(jq -r '.[].host' byob_secrets.json))
 # tokens=($(jq -r '.[].byob_token' byob_secrets.json))
